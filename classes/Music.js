8d0f03b25e215024e50e339a43f1e9bfbb14bbe2
Magnan Sylla <sylmar956@gmail.com>
2023-11-07 17:17:01 -0500

    Magnan add Music class (#3)
    * class Music
    
    * Music Class

--- classes/Music.js
+++ classes/Music.js
@@ -1,8 +1,32 @@
 // import the Media class:
+const Media =require("./Media")
 
 // create your Music class:
-class Music {
+class Music extends Media{
+    constructor(title, year, genre, artist,length ){
+    super(title, year, genre)
+    this.artist=artist
+    this.length=length
     
+    }
+    summary(){
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
+    }
+    static shortestAlbum(arrMusic){
+        let lowest = 999999;
+        let idx = 0;
+
+    arrMusic.forEach((song, i) => {
+      if (song.length < lowest) {
+        lowest = song.length;
+        idx = i;
+      }
+    
+    }
+    );
+
+    return arrMusic[idx];
+    }
 }
 
 // don't change below
--- main.js
+++ main.js
@@ -4,3 +4,5 @@ const Media = require("./classes/Media");
 const Book = require("./classes/Book");
 
 const Movie = require("./classes/Movie");
+
+const Music = require("./classes/Music");
