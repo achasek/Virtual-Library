51516e8df1ccccf134ab75813a451399b5fecbe0
Magnan <sylmar956@gmail.com>
2023-11-07 17:01:55 -0500

    class Music

--- classes/Music.js
+++ classes/Music.js
@@ -1,8 +1,31 @@
 // import the Media class:
+const Media =require("./Media")
 
 // create your Music class:
-class Music {
+class Music extends Media{
+    constructor(artist, length,title, year, genre ){
+    this.artist=artist
+    this.length=length
+    super(title, year, genre)
+    }
+    summary(){
+        return `"Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre:${this.genre}, Length: ${this.length}`
+    }
+    static shortestAlbum(arrMusic){
+        let lowest = 999999;
+    let idx = 0;
+
+    arrMusic.forEach((song, i) => {
+      if (song.duration < lowest) {
+        lowest = song.duration;
+        idx = i;
+      }
     
+    }
+    );
+
+    return arrMusic[idx];
+    }
 }
 
 // don't change below
