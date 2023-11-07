57f4d41b6adb18cef10eed5f3ba655ab2e049efe
Magnan <sylmar956@gmail.com>
2023-11-07 17:15:28 -0500

    Music Class

--- classes/Music.js
+++ classes/Music.js
@@ -3,21 +3,22 @@ const Media =require("./Media")
 
 // create your Music class:
 class Music extends Media{
-    constructor(artist, length,title, year, genre ){
+    constructor(title, year, genre, artist,length ){
+    super(title, year, genre)
     this.artist=artist
     this.length=length
-    super(title, year, genre)
+    
     }
     summary(){
-        return `"Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre:${this.genre}, Length: ${this.length}`
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
     }
     static shortestAlbum(arrMusic){
         let lowest = 999999;
-    let idx = 0;
+        let idx = 0;
 
     arrMusic.forEach((song, i) => {
-      if (song.duration < lowest) {
-        lowest = song.duration;
+      if (song.length < lowest) {
+        lowest = song.length;
         idx = i;
       }
     
